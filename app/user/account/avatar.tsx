3db78ab8109219f6538304e108eb9b314b54ec15
PoweredDeveloper <orokana.otaku.dev@gmail.com>
2024-01-24 01:24:19 +0300

    🔒 Authentication Fix

--- app/user/account/avatar.tsx
+++ app/user/account/avatar.tsx
@@ -83,10 +83,9 @@ export default function Avatar({
           accept="image/*"
           onChange={uploadAvatar}
           disabled={uploading}
-          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
-          {uploading ? 'Uploading ...' : 'Change'}
-        </input>
+          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-2"
+        />
+        {uploading ? 'Uploading ...' : ''}
       </div>
     </div>
   )
