4b8fe5fff4e5c1d49bcb1848aeb47f62d25fc396
PoweredDeveloper <orokana.otaku.dev@gmail.com>
2024-01-25 18:28:30 +0300

    🔒 Authentication Test

--- app/user/account/avatar.tsx
+++ app/user/account/avatar.tsx
@@ -1,16 +1,39 @@
 'use client'
 import { FaRegCircleUser } from "react-icons/fa6";
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 
 export default function Avatar({url}: {url: string | null}) {
+  const supabase = createClientComponentClient()
+    const [avatarUrl, setAvatarUrl] = useState<string | null>(null)
+  
+    useEffect(() => {
+      async function downloadImage(path: string) {
+        try {
+          const { data, error } = await supabase.storage.from('avatars').download(path)
+          if (error) {
+            throw error
+          }
+  
+          const url: string = URL.createObjectURL(data)
+          setAvatarUrl(url)
+        } catch (error) {
+          console.log('Error downloading image: ', error)
+        }
+      }
+  
+      if (url) downloadImage(url)
+    }, [url, supabase])
+
   return (
     <div className="col-span-full">
       <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
         Photo
       </label>
       <div className="mt-2 flex items-center gap-x-3">
-        {url
-          ? <Image alt="User Icon" width={128} height={128} src={url} className='max-h-12 max-w-12 rounded-full' />
+        {avatarUrl
+          ? <Image alt="User Icon" width={128} height={128} src={avatarUrl} className='max-h-12 max-w-12 rounded-full' />
           : <FaRegCircleUser className="h-12 w-12 text-gray-300" aria-hidden="true" />
         }
         <input
