86674a30c645cfecfbbe5f72917110563475e4a4
PoweredDeveloper <orokana.otaku.dev@gmail.com>
2024-01-29 00:40:19 +0300

    🔒 FIx Auth Form

--- app/user/account/account-form.tsx
+++ app/user/account/account-form.tsx
@@ -118,7 +118,7 @@ export default function AccountForm({user}: {user: User | null}) {
                   value={username || ''}
                   onChange={(e) => setUsername(e.target.value)}
                   autoComplete="username"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  className="w-full block flex-1 border-0 bg-transparent py-1.5 pl-0.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
