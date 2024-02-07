61678ee42b805574c132924db75892de83c8c5b9
PoweredDeveloper <orokana.otaku.dev@gmail.com>
2024-02-08 01:18:35 +0300

    🍃 Decorative Update #2.3.6

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -233,7 +233,7 @@ export default function Header({user}: {user: User | null}) {
           onClose={setMobileMenuOpen}
         >
           <div className="fixed inset-0 z-10" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-brown-900/10">
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-5 py-5 sm:max-w-sm sm:ring-1 sm:ring-brown-900/10">
             <div className="flex items-center justify-between">
               <Link href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Librairy</span>
